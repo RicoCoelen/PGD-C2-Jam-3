3dfe99fc6a426ac0b400ed228783946c58c93db4
ThomasO02 <thomas.otte02@gmail.com>
2019-09-30 16:20:49 +0200

    Added player script with movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public const float startHealth = 100;
+    public const float speed = 10;
+    float health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = Vector2.zero;
+
+        if (Input.GetKey("w"))
+        {
+            velocity.y += 1;
+        }
+        if (Input.GetKey("a"))
+        {
+            velocity.x -= 1;
+        }
+        if (Input.GetKey("s"))
+        {
+            velocity.y -= 1;
+        }
+        if (Input.GetKey("d"))
+        {
+            velocity.x += 1;
+        }
+
+        velocity.Normalize();
+        velocity *= speed;
+    }
+}
