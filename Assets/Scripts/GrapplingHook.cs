bfc0171f04d1e7319ba7e51b13d29d8f485bfbc7
lucal <louwril@hva.nl>
2019-10-01 15:32:48 +0200

    GrapplingHook janky stuff maar tis soort van iets

---
+++ Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=BXTjsCERdsw dit werkt misschien beter
+public class GrapplingHook : MonoBehaviour
+{
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float maxDistance;
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.LogWarning("Key pressed");
+
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, maxDistance, mask);
+
+            // For now grapple to anything, if not wanted add gameobject.getcomponent like hit.collider
+            if (hit.collider != null)
+            {
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+            }
+        }
+        if (Input.GetKeyUp("E"))
+        {
+            joint.enabled = false;
+        }
+    }
+}
