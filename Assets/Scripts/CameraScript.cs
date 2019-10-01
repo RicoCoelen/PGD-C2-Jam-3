2967aa93007c831de9a89d3c9e1716c3e94a2ac4
ThomasO02 <thomas.otte02@gmail.com>
2019-10-01 11:20:29 +0200

    Fixed camera to follow player

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPosition;
+        transform.Translate(player.transform.position.x, player.transform.position.y, 0);
+    }
+}
