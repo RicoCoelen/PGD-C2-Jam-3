54d6951a7d2c75ced1742721caf331ebd4f39c19
lucal <louwril@hva.nl>
2019-10-01 23:00:50 +0200

    GrapplingHook goes 360 around, changed camera to orthographic

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -9,40 +9,58 @@ public class GrapplingHook : MonoBehaviour
     Vector3 targetPos;
     RaycastHit2D hit;
     public float maxDistance=10f;
+    float distance;
+
     public LayerMask mask;
+    public LineRenderer line;
 
     // Start is called before the first frame update
     void Start()
     {
         joint = GetComponent<DistanceJoint2D>();
         joint.enabled = false;
+        line.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && joint.enabled == false)
+        if (Input.GetMouseButtonDown(0) /*&& joint.enabled == false*/)
         {
-            Debug.LogWarning("Key pressed");
-
             targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPos.z = 0;
 
-            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, maxDistance, mask);
+            //Detects where it hits a object
+               hit = Physics2D.Raycast(transform.position, (targetPos - transform.position).normalized, maxDistance, mask);
 
-            // For now grapple to anything, if not wanted add gameobject.getcomponent like hit.collider
             if (hit.collider != null)
             {
+                // "Creates" the joint and fixes it to the right positions
                 joint.enabled = true;
-                Debug.Log(hit.point);
                 joint.connectedAnchor = hit.point;
                 joint.distance = Vector2.Distance(transform.position, hit.point);
+
+                // "Creates" the line and fixes it to the right positions
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, hit.point);
             }
         }
+
         if (Input.GetMouseButtonDown(1) && joint.enabled == true)
         {
             joint.enabled = false;
+            line.enabled = false;
             Debug.Log("joint disabled");
         }
+
+        distance = Input.GetAxisRaw("Mouse ScrollWheel");
+
+        joint.distance += distance * 2f;
+
+        if (line.enabled == true)
+        {
+            line.SetPosition(0, transform.position);
+        }
+
     }
 }
