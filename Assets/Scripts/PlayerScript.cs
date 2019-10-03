a61311a5edeac529da805ed073ae042380694a42
lucal <louwril@hva.nl>
2019-10-03 12:17:38 +0200

    Ding

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -45,7 +45,7 @@ public class PlayerScript : MonoBehaviour
         }
 
         // check space for jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             // jump
             rb.AddForce(jump * jumpForce, ForceMode2D.Force);
